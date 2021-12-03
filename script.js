ad2a3c27cfadf396d42d5aa982a7bd64a93c43b3
Abhishek Choudhary <abhi@Abhisheks-MacBook-Pro.local>
2021-12-03 16:30:55 +0530

    minor changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ input = document.querySelector('input')
 button = document.querySelector('button')
 output = document.querySelector('.message')
 
+// this is event listener
 button.addEventListener("click", () =>{
     tip = input.value*10/100
     output.innerText = `Yoour Tip is ${tip}`
